ff892fdb2c26aa4ccce64b848cf0f373933f6d46
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-05-17 14:22:45 +0300

    My commit message

--- javascript/Dashboard.js
+++ javascript/Dashboard.js
@@ -1,37 +1,299 @@
-let apiProductApi = new TempApi.ProductApi();import TempApi from '../src/index';window.onload = () => {apiProductApi.getAllproduct((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("iv3vc").querySelectorAll( "[dataitem='true']" )].filter(
+let apiProductApi = new TempApi.ProductApi();import TempApi from '../src/index';const onClickPaginationButton = (chunk, pagination) => {
+    for (let i = 0; i < pagination.children.length; i++) {
+      if (
+        pagination.children[i].classList.value.includes("active") === true
+      ) {
+        pagination.children[i].classList.remove("active");
+      }
+    }
+
+    let numberOfFrontButtons =  findTypeOfPagination(pagination);
+    pagination.children[chunk+numberOfFrontButtons-1].classList.add("active");
+
+  apiProductApi.getAllproduct((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("iv3vc").querySelectorAll( "[dataitem='true']" )].filter(
     (element, index, array) =>
     !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
-  );const map = new Map();  data.forEach((item,i) => {
-    if(subDataElements.length > i)
-      {
-        try { 
-const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'pImage']");
+  );const map = new Map();[...subDataElements].forEach((element, index) => {
+        if (index >= data.length - (chunk-1)*subDataElements.length) {
+            subDataElements[index].style.display = 'none';
+        }
+        else {
+            subDataElements[index].style.display = "";
+        }
+      });data.forEach((item, i) => {
+
+        let revertIndex = data.length - i -1;
+
+        if(data.length - chunk*subDataElements.length <= revertIndex && revertIndex < data.length - (chunk-1)*subDataElements.length){
+            try { 
+const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'pImage']");
 if(insideSubDataElement !== null && data[data.length -i -1].pImage !== undefined){
   insideSubDataElement.src = data[data.length -i -1].pImage.data;
   insideSubDataElement.name = data[data.length -i -1].pImage.name;
 }
-else if(subDataElements[i].getAttribute('annotationname') === 'pImage' && data[data.length -i -1].pImage !== undefined){
-  subDataElements[i].src = data[data.length -i -1].pImage.data;
-  subDataElements[i].name = data[data.length -i -1].pImage.name;
+else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'pImage' && data[data.length -i -1].pImage !== undefined){
+  subDataElements[i-(chunk-1) *subDataElements.length].src = data[data.length -i -1].pImage.data;
+  subDataElements[i-(chunk-1) *subDataElements.length].name = data[data.length -i -1].pImage.name;
 }
  } catch (e) { console.log(e) };try { 
-      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'pTitle']");
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'pTitle']");
       if(insideSubDataElement !== null){
         insideSubDataElement.textContent = data[data.length -i -1].pTitle;
         
       }
-      else if(subDataElements[i].getAttribute('annotationname') === 'pTitle'){
-        subDataElements[i].textContent = data[data.length -i -1].pTitle;
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'pTitle'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].pTitle;
         
       }
      } catch (e) { console.log(e) };try { 
-      const insideSubDataElement = subDataElements[i].querySelector("[annotationname = 'pCount']");
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'pCount']");
       if(insideSubDataElement !== null){
         insideSubDataElement.textContent = data[data.length -i -1].pCount;
         
       }
-      else if(subDataElements[i].getAttribute('annotationname') === 'pCount'){
-        subDataElements[i].textContent = data[data.length -i -1].pCount;
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'pCount'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].pCount;
+        
+      }
+     } catch (e) { console.log(e) };
+            map.set(subDataElements[i-(chunk-1) * subDataElements.length].getAttribute('id'), data[data.length-i-1])
+        }
+        window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));
+    })
+    }});}
+
+    const findTypeOfPagination = (pagination) => {
+
+      let firstChild = pagination.children[0];
+      let secondChild = pagination.children[1];
+
+      if (
+        (firstChild.attributes.getNamedItem("pagination-first") !== null ||
+          firstChild.attributes.getNamedItem("pagination-previous") !== null) &&
+        (secondChild.attributes.getNamedItem("pagination-first") !== null ||
+          secondChild.attributes.getNamedItem("pagination-previous") !== null)
+      ) {
+        return 2;
+      } else if  (
+        (firstChild.attributes.getNamedItem("pagination-first") !== null ||
+          firstChild.attributes.getNamedItem("pagination-previous") !== null) ||
+        (secondChild.attributes.getNamedItem("pagination-first") !== null ||
+          secondChild.attributes.getNamedItem("pagination-previous") !== null)
+      ) {
+        return 1;
+      }
+      else{
+        return 0;
+      }
+
+    }
+  
+
+    const returnChunkIndex = (chunk, numberOfPages, cause) => {
+
+      if(cause === '+'){
+        if(chunk < numberOfPages){
+          return chunk + 1;
+        }
+        else{
+          return chunk;
+        }
+      }
+      else if(cause === '-'){
+        if(chunk > 2){
+          return chunk - 1;
+        }
+        else{
+          return 1;
+        }
+      }
+    }
+  document.getElementById('is5ler').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("is5ler")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};document.getElementById('ilc0yd').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ilc0yd")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};document.getElementById('ix6w3l').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("ix6w3l")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};document.getElementById('iyr8m1').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("iyr8m1")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};document.getElementById('i6mvrl').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("i6mvrl")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};document.getElementById('i7y94d').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("i7y94d")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};document.getElementById('i9969').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("i9969")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};document.getElementById('i8ahh').onclick = (event) => {
+    event.preventDefault();
+    let productId = window.location.pathname.replace('/Dashboard/','');
+      if(productId === '/Dashboard' || productId === ''){
+        let parentId = "";
+        const storedData = window.localStorage.getItem('data');
+        const newMap = new Map(JSON.parse(storedData));
+        newMap.forEach((value, key) => {
+          if (
+            document
+              .getElementById(key)
+              .contains(document.getElementById("i8ahh")) === true &&
+              document.getElementById(key).contains(document.getElementById(parentId)) === false
+          ) {
+            productId = value._id;
+            parentId = key;
+          }
+        });
+      }
+    apiProductApi.deleteproduct( productId, (error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully.');{  location.href= '/Dashboard' ;}}});};window.onload = () => {apiProductApi.getAllproduct((error, data, response) => { if (error) {console.error(error);} else { console.log('API called successfully. Returned data: ' + data); const subDataElements =[...document.getElementById("iv3vc").querySelectorAll( "[dataitem='true']" )].filter(
+    (element, index, array) =>
+    !array.reduce((hasAncestorFlag, dataItem) => hasAncestorFlag || (element.compareDocumentPosition(dataItem) & Node.DOCUMENT_POSITION_CONTAINS) === 8, false)
+  );const map = new Map();let chunk = 1;  data.forEach((item,i) => {
+    if(subDataElements.length > i)
+      {
+        try { 
+const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'pImage']");
+if(insideSubDataElement !== null && data[data.length -i -1].pImage !== undefined){
+  insideSubDataElement.src = data[data.length -i -1].pImage.data;
+  insideSubDataElement.name = data[data.length -i -1].pImage.name;
+}
+else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'pImage' && data[data.length -i -1].pImage !== undefined){
+  subDataElements[i-(chunk-1) *subDataElements.length].src = data[data.length -i -1].pImage.data;
+  subDataElements[i-(chunk-1) *subDataElements.length].name = data[data.length -i -1].pImage.name;
+}
+ } catch (e) { console.log(e) };try { 
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'pTitle']");
+      if(insideSubDataElement !== null){
+        insideSubDataElement.textContent = data[data.length -i -1].pTitle;
+        
+      }
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'pTitle'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].pTitle;
+        
+      }
+     } catch (e) { console.log(e) };try { 
+      const insideSubDataElement = subDataElements[i-(chunk-1) *subDataElements.length].querySelector("[annotationname = 'pCount']");
+      if(insideSubDataElement !== null){
+        insideSubDataElement.textContent = data[data.length -i -1].pCount;
+        
+      }
+      else if(subDataElements[i-(chunk-1) *subDataElements.length].getAttribute('annotationname') === 'pCount'){
+        subDataElements[i-(chunk-1) *subDataElements.length].textContent = data[data.length -i -1].pCount;
         
       }
      } catch (e) { console.log(e) };
@@ -43,4 +305,91 @@ else if(subDataElements[i].getAttribute('annotationname') === 'pImage' && data[d
 
     window.localStorage.setItem('data', JSON.stringify(Array.from(map.entries())));
     
+  let numberOfPages = Math.ceil(data.length/subDataElements.length);
+  let pagination = document.querySelector('[pagination-list="true"]');
+
+    let paginationAttributes = [
+      "pagination-first",
+      "pagination-last",
+      "pagination-previous",
+      "pagination-next",
+    ];
+
+    for (let i = 0; i < pagination.children.length; ) {
+      let foundAttr = false;
+
+      paginationAttributes.forEach((attr) => {
+        if (pagination.children[i].attributes.getNamedItem(attr) !== null) {
+          foundAttr = true;
+        }
+      });
+      if (foundAttr === false) {
+        pagination.removeChild(pagination.children[i]);
+      } else {
+        i++;
+      }
+    }
+
+    for (let i = 0; i < numberOfPages; i++) {
+      let child = document.createElement("li");
+      child.classList.add("page-item");
+      if (i === numberOfPages - 1) {
+        child.classList.add("active");
+      }
+      let insideChild = document.createElement("a");
+      insideChild.classList.add("page-link");
+      let textnode = document.createTextNode(numberOfPages - i);
+      insideChild.appendChild(textnode);
+      insideChild.setAttribute("href", "#!");
+      child.appendChild(insideChild);
+      child.onclick = function () {
+        if (chunk !== numberOfPages - i) {
+          chunk = numberOfPages - i;
+          onClickPaginationButton(chunk, pagination);
+        }
+      };
+
+      let numberOfFrontButtons =  findTypeOfPagination(pagination);
+      pagination.insertBefore(child, pagination.children[numberOfFrontButtons]);
+
+    }
+
+    for (let i = 0; i < pagination.children.length; i++) {
+      let child = pagination.children[i];
+      if ( child.getAttribute("pagination-first") === "true" && numberOfPages > 0 ) {
+        child.onclick = function () {
+          if(chunk !== 1){
+            chunk = 1;
+            onClickPaginationButton(1, pagination);
+          }
+        };
+      }
+
+      if ( child.getAttribute( "pagination-last" ) === "true" && numberOfPages > 0 ) {
+        child.onclick = function () {
+          if(chunk !== numberOfPages){
+            chunk = numberOfPages;
+            onClickPaginationButton( numberOfPages, pagination);
+          }
+        };
+      }
+
+      if ( child.getAttribute("pagination-previous") === "true" && numberOfPages > 0 ) {
+        child.onclick = function () {
+          if(chunk !== returnChunkIndex(chunk,numberOfPages,'-')){
+            chunk = returnChunkIndex(chunk,numberOfPages,'-');
+            onClickPaginationButton(chunk, pagination);
+          }
+        };
+      }
+
+      if ( child.getAttribute("pagination-next") === "true" && numberOfPages > 0) {
+        child.onclick = function () {
+          if(chunk !== returnChunkIndex(chunk,numberOfPages,'+')){
+            chunk = returnChunkIndex(chunk,numberOfPages,'+');
+            onClickPaginationButton(chunk, pagination);
+          }
+        };
+      }
+    };
     [...subDataElements].forEach((element,index) => {if(index >= data.length) subDataElements[index].style.display = 'none';})}});};
